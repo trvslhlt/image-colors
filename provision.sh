870fc4fdd39eaf84efcd0bbaf89c2e8909f37124
trvslhlt <trvslhlt@gmail.com>
2016-11-22 12:29:27 -0500

    Nginx
    - create config file
    - cp the config
    - close port for go app in vagrant file

--- provision.sh
+++ provision.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 sudo su -
 
 apt-get update
@@ -6,8 +8,13 @@ apt-get -y install nginx
 apt-get -y install golang-go
 apt-get -y install imagemagick
 
-echo "export GOPATH=\"/usr/share/go/\"" >> .profile
-. .profile
+cp /vagrant/nginx_sites_enabled_default /etc/nginx/sites-enabled/default
+sudo service nginx restart
+
+echo "export GOPATH=/usr/share/go/" >> .profile
+echo "export GOBIN=$GOPATH/bin" >> .profile
+echo "export PATH=$GOPATH/bin:$PATH" >> .profile
+. ~/.profile
 
 go get github.com/gorilla/mux
 go get github.com/gorilla/handlers
