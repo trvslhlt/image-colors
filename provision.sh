c26e657fccfa2607cc4d6fc18037f9a28b3054b0
trvslhlt <trvslhlt@gmail.com>
2016-11-26 16:40:16 -0500

    Set and store GO env vars

--- provision.sh
+++ provision.sh
@@ -1,21 +1,38 @@
 #!/bin/sh
 
+
+# act as super user
 sudo su -
 
+
+# install tools
 apt-get update
 apt-get -y install git
 apt-get -y install nginx
 apt-get -y install golang-go
 apt-get -y install imagemagick
 
-cp /vagrant/nginx_sites_enabled_default /etc/nginx/sites-enabled/default
-sudo service nginx restart
 
-echo "export GOPATH=/usr/share/go/" >> .profile
-echo "export GOBIN=$GOPATH/bin" >> .profile
-echo "export PATH=$GOPATH/bin:$PATH" >> .profile
-. ~/.profile
+# configure go environment
+export GOPATH=/usr/share/go
+export GOBIN=$GOPATH/bin
+echo "export GOPATH=$GOPATH" >> .profile
+echo "export GOBIN=$GOBIN" >> .profile
 
+
+# get go app dependencies
 go get github.com/gorilla/mux
 go get github.com/gorilla/handlers
 go get github.com/asaskevich/govalidator
+
+
+# configure web server
+# cp /vagrant/nginx_sites_enabled_default /etc/nginx/sites-enabled/default
+# service nginx restart
+
+# configure process management tool
+# addgroup --system supervisor
+# adduser vagrant supervisor
+# cp /vagrant/supervisord.conf /etc/supervisor/supervisord.conf
+# cp /vagrant/app_supervisor.conf /etc/supervisor/conf.d/app.conf
+# supervisorctl reload
