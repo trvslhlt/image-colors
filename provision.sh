9cd03b56df4428e1a950731de49bc2ac0a2b1813
trvslhlt <trvslhlt@gmail.com>
2016-11-26 17:33:26 -0500

    Attempting to replicate manual supervisor config

--- provision.sh
+++ provision.sh
@@ -11,6 +11,7 @@ apt-get -y install git
 apt-get -y install nginx
 apt-get -y install golang-go
 apt-get -y install imagemagick
+apt-get -y install supervisor
 
 
 # configure go environment
@@ -26,13 +27,21 @@ go get github.com/gorilla/handlers
 go get github.com/asaskevich/govalidator
 
 
+# perpare app
+cp /vagrant/image-colors-app/image-colors-app.go ~/image-colors-app.go
+go build image-colors-app.go
+
+
 # configure web server
-# cp /vagrant/nginx_sites_enabled_default /etc/nginx/sites-enabled/default
-# service nginx restart
+cp /vagrant/nginx_sites_enabled_default /etc/nginx/sites-enabled/default
+service nginx restart
+echo "need to add nginx to supervisor"
+
 
 # configure process management tool
-# addgroup --system supervisor
-# adduser vagrant supervisor
-# cp /vagrant/supervisord.conf /etc/supervisor/supervisord.conf
-# cp /vagrant/app_supervisor.conf /etc/supervisor/conf.d/app.conf
-# supervisorctl reload
+addgroup --system supervisor
+adduser vagrant supervisor
+cp /vagrant/supervisor/supervisord.conf /etc/supervisor/supervisord.conf
+cp /vagrant/supervisor/image-colors-app.conf /etc/supervisor/conf.d/image-colors-app.conf
+service supervisor restart
+supervisorctl reload
